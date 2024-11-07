9918023a9b43135115f0aa82de99bd8ff45451f9
ARK0421 <wodbs5188@naver.com>
2024-11-07 11:44:35 +0900

    member_project 생성

---
+++ member_project/frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Header from "./Header";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/save">회원 가입</Route>
+          <Route path="/list">회원 목록</Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
