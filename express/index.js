55fdcc27ee86c251b3f145b01caf8d51df625c14
ARK0421 <wodbs5188@naver.com>
2024-11-07 10:25:32 +0900

    update put

--- express/index.js
+++ express/index.js
@@ -84,3 +84,27 @@ app.get("/nations/:id", (req, res) => {
     }
   });
 });
+
+// 저장기능
+app.post("/nations/save", (req) => {
+  const { name, capital, population } = req.body;
+  console.log(
+    `name : ${name}, capital : ${capital}, population : ${population}`
+  );
+  const sql =
+    "insert into nations_table(name, capital, population) values(?,?,?)";
+
+  db.query(sql, [name, capital, population], (err, results, fields) => {
+    console.log("err", err);
+  });
+});
+
+// 수정
+app.put("/nations/:id", (req, res) => {
+  const { id, name, capital, population } = req.body;
+  const sql = "update nations_table set population=? where id=?";
+  db.query(sql, [population, id], (err, results, fields) => {
+    console.log("err", err);
+    res.status(200);
+  });
+});
