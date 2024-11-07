e92582e6402894c785cd01b41a62ae9b5f1a5bd8
ARK0421 <wodbs5188@naver.com>
2024-11-07 11:53:42 +0900

    save.jsx 생성

--- member_project/frontend/src/App.jsx
+++ member_project/frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Header from "./Header";
+import Save from "./Save";
 import "./App.css";
 
 function App() {
@@ -11,7 +12,9 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/save">회원 가입</Route>
+          <Route path="/save" element={<Save />}>
+            회원 가입
+          </Route>
           <Route path="/list">회원 목록</Route>
         </Routes>
       </BrowserRouter>
---
+++ member_project/frontend/src/Save.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+const Save = () => {
+  return (
+    <>
+      <h2>Save.jsx</h2>
+      E-mail : <input type="text" />
+    </>
+  );
+};
+
+export default Save;
