6ca60e55ccb7afff2cb451e06d93b1e7eb3710c7
ARK0421 <wodbs5188@naver.com>
2024-11-12 11:38:47 +0900

    memberList 조회

--- member_project/frontend/src/pages/List.jsx
+++ member_project/frontend/src/pages/List.jsx
@@ -11,11 +11,31 @@ const List = () => {
 
         setList(res.data);
       });
-  });
+  }, []);
 
   return (
     <>
       <h2>List.jsx</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>email</th>
+            <th>name</th>
+            <th>mobile</th>
+          </tr>
+        </thead>
+        <tbody>
+          {list.map((member) => (
+            <tr key={member.id}>
+              <td>{member.id}</td>
+              <td>{member.member_email}</td>
+              <td>{member.member_name}</td>
+              <td>{member.member_mobile}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 };
