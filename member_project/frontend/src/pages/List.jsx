c145388682495835aff7067a85b9d34a130c11f5
ARK0421 <wodbs5188@naver.com>
2024-11-12 11:19:37 +0900

    useEffect 작성

--- member_project/frontend/src/pages/List.jsx
+++ member_project/frontend/src/pages/List.jsx
@@ -1,9 +1,23 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const List = () => {
-    return (
-        <>
-            <h2>List.jsx</h2>
-        </>
-    )
-}
+  const [list, setList] = useState([]);
+  useEffect(() => {
+    const list_db = axios
+      .get("http://localhost:8027/member/list")
+      .then((res) => {
+        console.log("res", res.data);
+
+        setList(res.data);
+      });
+  });
+
+  return (
+    <>
+      <h2>List.jsx</h2>
+    </>
+  );
+};
 
 export default List;
